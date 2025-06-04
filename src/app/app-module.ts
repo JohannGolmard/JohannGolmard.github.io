baffa312be7dc775d7d5ece2950e9961a7c95a23
unknown <johann.golmard@gmail.com>
2025-06-04 17:47:16 +0200

    Reorganize project, added footer.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,13 +7,17 @@ import { App } from './app';
 import { Home } from './home/home';
 import { ScrollReveal } from './scroll-reveal';
 import { NetworkBackground } from './network-background/network-background';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
 
 @NgModule({
   declarations: [
     App,
     Home,
     ScrollReveal,
-    NetworkBackground
+    NetworkBackground,
+    Navbar,
+    Footer
   ],
   imports: [
     BrowserModule,
